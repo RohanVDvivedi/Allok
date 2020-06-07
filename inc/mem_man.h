a7abaa4d98557c6f34780badc9371410e5a78d64
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-06-07 17:55:01 +0530

    started working on allok bst memory allocator

--- inc/mem_man.h
+++ inc/mem_man.h
@@ -1,78 +1,8 @@
 #ifndef MEM_MAN_H
 #define MEM_MAN_H
 
-#include<stdint.h>
+void* allok(size_t size);
 
-#define NULL 0
-
-typedef struct mem_man mem_man;
-struct mem_man
-{
-	unsigned long long int memory_size;
-
-	// this is the root of the binary search tree that maintains the free blocks
-	void* root;
-
-	// this is the first block in the memory manager
-	void* first_block;
-
-	// it is the largest free memory that we can provide to the user
-	// if the user happens to ask for a block larger than this size, we return NULL from allok
-	unsigned long long int largest_free_blk_size;
-};
-
-/*
-	structure of a memory block is as below
-	{
-		mem_man block
-	}
-	{
-		{
-			mem_blk_prefix
-		}
-		{
-			actual free memory lies here
-		}
-		{
-			mem_blk_suffix
-		}
-	}
-*/
-
-typedef struct mem_blk_prefix mem_blk_prefix;
-struct mem_blk_prefix
-{
-	// the address of the parent block of this block in balanced binary search tree of free blocks
-	void* parent;
-
-	// the address of the left block of this block in balanced binary search tree of free blocks
-	void* left;
-
-	// the address of the right block of this block in balanced binary search tree of free blocks
-	void* right;
-
-	// size of the block of this memory, it is the size excluding the mem_blk_prefix and mem_blk_suffix size
-	// it is the free memory that the user can use or is using
-	// negative size indicated that block in not free
-	int32_t blk_size;
-};
-
-typedef struct mem_blk_suffix mem_blk_suffix;
-struct mem_blk_suffix
-{
-	// size of the block of this memory, it is the size excluding the mem_blk_prefix and mem_blk_suffix size
-	// it is the free memory that the user can use or is using
-	// negative size indicated that block in not free
-	int32_t blk_size;
-};
-
-mem_man* mem_man_init(void* memory, uint32_t memory_size);
-
-// to allocate memory of given size on provided memory manager's context
-void* allok(mem_man* mem_man_p, uint32_t size);
-
-// to free memory on provided memory manager's context, 
-// by giving back the same memory pointer that was returned by allok 
-void freek(mem_man* mem_man_p, void* mem);
+void freek(void* mptr);
 
 #endif
--- src/mem_man.c
+++ src/mem_man.c
@@ -1,18 +1,96 @@
 #include<mem_man.h>
 
-mem_man* mem_man_init(void* memory, uint32_t memory_size)
+#include<bst.h>
+
+// lock to protect the tree of nodes
+static pthread_mutex_t lock;
+
+// binary search tree to link free nodes, so we can find best fit free nodes
+static bst free_tree;
+
+typedef struct header header;
+struct header
 {
-	// TODO
-	return NULL;
+	size_t total_size;
+	bstnode free_node;
 }
 
-void* allok(mem_man* mem_man_p, uint32_t size)
+typedef struct footer footer;
+struct footer
 {
-	// TODO
-	return NULL;
+	int is_free;
+	size_t total_size;
 }
 
-void freek(mem_man* mem_man_p, void* mem)
+#define get_header_from_mptr(mptr) (((void*)(mptr)) - sizeof(header))
+#define get_mptr_from_header(hedr) (((void*)(hedr)) + sizeof(header))
+
+#define get_footer_from_header(hedr) (((void*)(hedr)) + sizeof(header) + ((header*)(hedr))->size)
+#define get_header_from_footer(futr) (((void*)(futr)) - ((footer*)(futr))->size - sizeof(header))
+
+// if you request block smaller than this you will get MIN_BLOCK_SIZE
+#define MIN_BLOCK_SIZE  (sizeof(header) + sizeof(footer) + 8)
+
+void allok_init(size_t size)
 {
+	pthread_mutex_init(&lock, NULL);
+	initialize_bst(&free_tree);
+}
+
+void init_block(void* block, size_t total_size)
+{
+	header* blockH = block;
+	footer* blockF = block + total_size - sizeof(footer);
+	blockH->total_size = total_size;
+	initialize_bstnode(&(blockH->free_node));
+	blockF->is_free = 1;
+	blockF->total_size = total_size;
+}
+
+header* split(header* big_blockH, size_t split_at_size)
+{
+	header* split_blockH = (header*)(((void*)big_blockH) + split_at_size);
+	size_t split_block_size = big_blockH->size - split_at_size;
+
+	init_block(big_blockH, split_at_size);
+	init_block(split_blockH, split_block_size);
+
+	return split_blockH;
+}
+
+void* allok(size_t size)
+{
+	size_t total_size = sizeof(header) + size + sizeof(footer);
+
+	if(size < MIN_BLOCK_SIZE)
+	{
+		size = MIN_BLOCK_SIZE;
+	}
+
+	pthread_mutex_lock(&lock);
+
+	header* blockH = (header*) find_succeeding_or_equals(&free_tree, &total_size);
+	remove_from_bst(&free_tree, blockH);
+
+	if(blockH != NULL)
+	{
+		if(blockH->total_size > total_size + MIN_BLOCK_SIZE)
+		{
+			header* block_Splited = split(blockH, total_size);
+			insert_in_bst(&free_tree, block_Splited);
+		}
+		footer* blockF = get_footer_from_header(blockH);
+		blockF->is_free = 0;
+	}
+
+	pthread_mutex_unlock(&lock);
+
+	// TODO
+	return (blockH != NULL) ? get_mptr_from_header(blockH) : NULL;
+}
+
+void freek(void* mptr)
+{
+	header* blockH = get_header_from_mptr(mptr);
 	// TODO
 }
