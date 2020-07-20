772fb9fd8e4ed196d0a2ef6294c946108aa0e900
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-07-20 20:57:58 +0530

    completed revamped implementation of the non thread safe version of the allocator

--- inc/mem_man.h
+++ inc/mem_man.h
@@ -1,6 +1,11 @@
 #ifndef MEM_MAN_H
 #define MEM_MAN_H
 
+#include<stdlib.h>
+#include<stddef.h>
+
+void allok_init();
+
 void* allok(size_t size);
 
 void freek(void* mptr);
--- src/mem_man.c
+++ src/mem_man.c
@@ -2,95 +2,145 @@
 
 #include<bst.h>
 
-// lock to protect the tree of nodes
-static pthread_mutex_t lock;
-
 // binary search tree to link free nodes, so we can find best fit free nodes
 static bst free_tree;
 
-typedef struct header header;
-struct header
+typedef struct block_header block_header;
+struct block_header
 {
-	size_t total_size;
+	block_header* next;
+	block_header* prev;
 	bstnode free_node;
+	size_t payload_size;
+	char payload[];
+};
+
+int block_compare(const void* data1, const void* data2)
+{
+	block_header* h1 = (block_header*) data1;
+	block_header* h2 = (block_header*) data2;
+	if(h1->payload_size > h2->payload_size)
+		return 1;
+	else if(h1->payload_size < h2->payload_size)
+		return -1;
+	else
+		return 0;
 }
 
-typedef struct footer footer;
-struct footer
+#define MAX_BLOCK_SIZE (4096 * 2)
+#define MIN_BLOCK_SIZE (sizeof(block_header) + 8)
+
+#define MAX_PAYLOAD_SIZE MAX_BLOCK_SIZE - sizeof(block_header)
+#define MIN_PAYLOAD_SIZE MIN_BLOCK_SIZE - sizeof(block_header)
+
+void allok_init()
 {
-	int is_free;
-	size_t total_size;
-}
-
-#define get_header_from_mptr(mptr) (((void*)(mptr)) - sizeof(header))
-#define get_mptr_from_header(hedr) (((void*)(hedr)) + sizeof(header))
-
-#define get_footer_from_header(hedr) (((void*)(hedr)) + sizeof(header) + ((header*)(hedr))->size)
-#define get_header_from_footer(futr) (((void*)(futr)) - ((footer*)(futr))->size - sizeof(header))
-
-// if you request block smaller than this you will get MIN_BLOCK_SIZE
-#define MIN_BLOCK_SIZE  (sizeof(header) + sizeof(footer) + 8)
-
-void allok_init(size_t size)
-{
-	pthread_mutex_init(&lock, NULL);
-	initialize_bst(&free_tree);
+	initialize_bst(&free_tree, RED_BLACK_TREE, offsetof(block_header, free_node), block_compare);
 }
 
 void init_block(void* block, size_t total_size)
 {
-	header* blockH = block;
-	footer* blockF = block + total_size - sizeof(footer);
-	blockH->total_size = total_size;
+	block_header* blockH = block;
+	blockH->next = NULL;
+	blockH->prev = NULL;
 	initialize_bstnode(&(blockH->free_node));
-	blockF->is_free = 1;
-	blockF->total_size = total_size;
+	blockH->payload_size = total_size - sizeof(block_header);
 }
 
-header* split(header* big_blockH, size_t split_at_size)
+block_header* get_new_block()
 {
-	header* split_blockH = (header*)(((void*)big_blockH) + split_at_size);
-	size_t split_block_size = big_blockH->size - split_at_size;
+	void* block = malloc(MAX_BLOCK_SIZE);
+	init_block(block, MAX_BLOCK_SIZE);
+	return block;
+}
 
-	init_block(big_blockH, split_at_size);
-	init_block(split_blockH, split_block_size);
+void return_block_memory(block_header* blockH)
+{
+	free(blockH);
+}
 
-	return split_blockH;
+int is_free_block(block_header* blockH)
+{
+	return exists_in_bst(&free_tree, blockH);
+}
+
+// returns the other splitted block header, if splitted
+block_header* split(block_header* big_blockH, size_t required_payload_size)
+{
+	if(big_blockH->payload_size < required_payload_size + MIN_BLOCK_SIZE)
+		return NULL;
+
+	big_blockH->payload_size = required_payload_size;
+
+	size_t new_block_size = big_blockH->payload_size - required_payload_size;
+	block_header* new_block = (block_header*)(big_blockH->payload + big_blockH->payload_size);
+	init_block(new_block, new_block_size);
+
+	new_block->prev = big_blockH;
+	new_block->next = big_blockH->next;
+
+	if(big_blockH->next != NULL)
+		big_blockH->next->prev = new_block;
+	big_blockH->next = new_block;
+
+	return new_block;
+}
+
+// merges blockH and its blockH->next header if it is not NULL
+int merge(block_header* blockH)
+{
+	if(blockH->next == NULL)
+		return 0;
+
+	blockH->payload_size = blockH->payload_size + sizeof(block_header) + blockH->next->payload_size;
+
+	blockH->next = blockH->next->next;
+	if(blockH->next->next != NULL)
+		blockH->next->next->prev = blockH;
+
+	return 1;
 }
 
 void* allok(size_t size)
 {
-	size_t total_size = sizeof(header) + size + sizeof(footer);
+	// cap to MIN_PAYLOAD_SIZE
+	if(size < MIN_PAYLOAD_SIZE)
+		size = MIN_PAYLOAD_SIZE;
 
-	if(size < MIN_BLOCK_SIZE)
+	block_header* blockH = NULL; // find suitable block from the free tree
+	if(blockH == NULL)
+		blockH = get_new_block();
+
+	if(blockH->payload_size > size)
 	{
-		size = MIN_BLOCK_SIZE;
+		block_header* splitted_block = split(blockH, size);
+		if(splitted_block != NULL)
+			insert_in_bst(&free_tree, splitted_block);
 	}
 
-	pthread_mutex_lock(&lock);
-
-	header* blockH = (header*) find_succeeding_or_equals(&free_tree, &total_size);
-	remove_from_bst(&free_tree, blockH);
-
-	if(blockH != NULL)
-	{
-		if(blockH->total_size > total_size + MIN_BLOCK_SIZE)
-		{
-			header* block_Splited = split(blockH, total_size);
-			insert_in_bst(&free_tree, block_Splited);
-		}
-		footer* blockF = get_footer_from_header(blockH);
-		blockF->is_free = 0;
-	}
-
-	pthread_mutex_unlock(&lock);
-
-	// TODO
-	return (blockH != NULL) ? get_mptr_from_header(blockH) : NULL;
+	return blockH;
 }
 
 void freek(void* mptr)
 {
-	header* blockH = get_header_from_mptr(mptr);
-	// TODO
+	block_header* blockH = (mptr - sizeof(block_header));
+
+	// if it is already free return the block
+	if(is_free_block(blockH))
+		return;
+
+	if(blockH->next != NULL && is_free_block(blockH->next))
+	{
+		remove_from_bst(&free_tree, blockH->next);
+		merge(blockH);
+	}
+
+	if(blockH->prev != NULL && is_free_block(blockH->prev))
+	{
+		remove_from_bst(&free_tree, blockH->prev);
+		blockH = blockH->prev;
+		merge(blockH);
+	}
+
+	insert_in_bst(&free_tree, blockH);
 }
