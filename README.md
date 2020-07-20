f1adb345610cc95771822f41a2da44863ce634a7
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-07-20 22:39:40 +0530

    added details for the test case, resoned for the lower memory efficiency and yup, use it at your own risk

--- README.md
+++ README.md
@@ -3,6 +3,15 @@ BST based generic memory allocator replacement for malloc, log(n) best fit algor
 
 Note: the alocator is thread-unsafe, use global locks in your application.
 
+ * Note: The allocator uses a lot of memory for management. Just for the idea see the analysis below:
+	* After running the test case on my machine with allocation sample sizes {6, 16, 20, 35, 55}.
+	  * total usable memory (acquired over 175 allocations) = 4701 bytes
+	  * total memory used by the allocator = 4 * 4096 bytes (each page used by the allocator is 16 KB)
+	  * total memory used by the allocator for memory management = 64 * 175 bytes ==> 11200 bytes
+      * efficiency of the test program = 28%
+ * The memory allocator is not very efficient with smaller memory allocations like 6 - 24 bytes, because each allocated block will use up 64 bytes for its own management internally to the allocator.
+ * Use this allocator should be used for larger block sizes for better efficiency.
+
 setup instructions
 
  * git clone https://github.com/RohanVDvivedi/Allok.git
--- test/test.c
+++ test/test.c
@@ -3,29 +3,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define TEST_ALLOKS 50
+#define TEST_ALLOKS 175
 
-int test_sizes_size = 5;
-int test_sizes[] = {6, 16, 20, 35, 55};
+unsigned int test_sizes_size = 5;
+unsigned int test_sizes[] = {6, 16, 20, 35, 55};
 
 int memory_sizes[TEST_ALLOKS];
 void* memory[TEST_ALLOKS];
 
 int main()
 {
+	unsigned int total_allocation_size = 0;
 	allok_init();
 
-	for(int i = 0; i < TEST_ALLOKS; i++)
+	for(unsigned int i = 0; i < TEST_ALLOKS; i++)
 	{
 		memory_sizes[i] = test_sizes[ rand() % test_sizes_size ];
 		printf("%d - Allocating memory for size %u\n", i, memory_sizes[i]);
 		memory[i] = allok(memory_sizes[i]);
 		printf("%d - Allocated memory for size %u at %p\n\n", i, memory_sizes[i], memory[i]);
+
+		total_allocation_size += memory_sizes[i];
 	}
 
 	printf("Allocations Completed\n");
 
-	for(int i = 0; i < TEST_ALLOKS; i++)
+	printf("Total usable memory size: %u\n", total_allocation_size);
+
+	for(unsigned int i = 0; i < TEST_ALLOKS; i++)
 	{
 		printf("%d - Freeing memory for size %u at %p\n\n", i, memory_sizes[i], memory[i]);
 		freek(memory[i]);
