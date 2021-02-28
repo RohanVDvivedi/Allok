90a5a5c8b89c5dad00065e9873b1a961f9b451e4
Rohan Dvivedi <rohandvivedi@gmail.com>
2021-02-28 09:55:05 +0530

    added necessary comments for public api usage

--- inc/allok.h
+++ inc/allok.h
@@ -3,10 +3,23 @@
 
 #include<stdlib.h>
 
+// this should be the first line of your main function for using this allocator
 void allok_init(int debugL);
 
+// allocates memory of given size and returns the pointer pointing to it
 void* allok(size_t size);
 
+// frees previously allocated memory block
 void freek(void* mptr);
+
+/*
+	General use case ::
+
+		void* mptr = allok( SIZE );
+
+		// your logic
+
+		freek(mptr);
+*/
 
 #endif
