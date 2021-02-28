1eb1fec6db921bb0ec9bb8d7966efcd90153b13f
Rohan Dvivedi <rohandvivedi@gmail.com>
2021-02-28 11:44:19 +0530

    scoping free_tree and blocks_list global static variables using orderly declaration, built get_total_block_size function and using it to reach end of block, fixed free tree insertion bug in freek

--- src/allok.c
+++ src/allok.c
@@ -12,9 +12,6 @@
 
 static int debug = 0;
 
-// binary search tree to link free nodes, so we can find best fit free nodes
-static bst free_tree;
-
 // doubly linked list to find next and previous memory blocks quickly
 static linkedlist blocks_list;
 
@@ -70,10 +67,9 @@ static block_header* get_new_block()
 	return block;
 }
 
-// returns 1, if a block is free
-static int is_free_block(const block_header* blockH)
+static size_t get_total_block_size(const block_header* blockH)
 {
-	return (!is_new_bstnode(&free_tree, &(blockH->free_node))) || (free_tree.root == &(blockH->free_node));
+	return sizeof(block_header) + blockH->payload_size;
 }
 
 // returns 1 if blockH1 and blockH2 are physically adjacent to each other
@@ -85,12 +81,12 @@ static int are_adjacent_blocks(const block_header* blockH1, const block_header* 
 
 	// conditions to meet for block ordering ::  blockH1 then blockH2
 	if( (get_next_of(&blocks_list, blockH1) == blockH2) &&
-		((blockH1->payload + blockH1->payload_size) == ((void*)blockH2)) )
+		((((void*)blockH1) + get_total_block_size(blockH1)) == ((void*)blockH2)) )
 		return 1;
 
 	// conditions to meet for block ordering ::  blockH2 then blockH1
 	if( (get_prev_of(&blocks_list, blockH1) == blockH2) &&
-		(((void*)blockH1) == (blockH2->payload + blockH2->payload_size)) )
+		((((void*)blockH2) + get_total_block_size(blockH2)) == ((void*)blockH1)) )
 		return 1;
 
 	// not adjacent blocks
@@ -103,7 +99,7 @@ static const block_header* get_next_block_of(const block_header* blockH)
 	const block_header* next_blockH = get_next_of(&blocks_list, blockH);
 
 	// check if this block is physically after blockH
-	if( (blockH->payload + blockH->payload_size) == ((void*)next_blockH) )
+	if((((void*)blockH) + get_total_block_size(blockH)) == ((void*)next_blockH))
 		return next_blockH;
 	return NULL;
 }
@@ -114,7 +110,7 @@ static const block_header* get_prev_block_of(const block_header* blockH)
 	const block_header* prev_blockH = get_prev_of(&blocks_list, blockH);
 
 	// check if this block is physically before blockH
-	if( (prev_blockH->payload + prev_blockH->payload_size) == ((void*)blockH) )
+	if((((void*)prev_blockH) + get_total_block_size(prev_blockH)) == ((void*)blockH))
 		return prev_blockH;
 	return NULL;
 }
@@ -138,36 +134,45 @@ static block_header* split(block_header* big_blockH, size_t required_payload_siz
 	big_blockH->payload_size = required_payload_size;
 
 	// find the new address of the split block
-	block_header* new_block = (block_header*)(big_blockH->payload + big_blockH->payload_size);
-	init_block(new_block, new_block_total_size);
+	block_header* new_blockH = (block_header*)(big_blockH->payload + big_blockH->payload_size);
+	init_block(new_blockH, new_block_total_size);
 
-	// insert the new block (the ) in the 
-	insert_after(&blocks_list, big_blockH, new_block);
+	// insert the new block in the blocks_list after the big_blockH
+	insert_after(&blocks_list, big_blockH, new_blockH);
 
-	return new_block;
+	return new_blockH;
 }
 
 // merges blockH and its blockH->next header if it is not NULL
 static int merge(block_header* blockH)
 {
-	// find the next block of the blockH
-	block_header* next_block = (block_header*) get_next_of(&blocks_list, blockH);
-
-	// single block in the linkedlist
-	// OR if the block_next is not physically adjacent to the block_next
-	// OR if the blockH's payload_size is equal to MAX_PAYLOAD_SIZE
-	// then => no merging possible
-	if( (next_block == blockH) || (blockH->payload + blockH->payload_size != ((char*)next_block))
-		|| (blockH->payload_size == MAX_PAYLOAD_SIZE) )
+	// find the next block of the blockH,
+	// fail the merge operation with a return 0 if it does not exist
+	const block_header* next_blockH = get_next_block_of(blockH);
+	if(next_blockH == NULL)
 		return 0;
 
-	// add block_header size and payload size to the blockH's payload_size
-	blockH->payload_size = blockH->payload_size + (sizeof(block_header) + next_block->payload_size);
+	// can not merge anything to the block of Allok's page sized memory block
+	if( ( blockH->payload_size      == MAX_PAYLOAD_SIZE ) || 
+		( next_blockH->payload_size == MAX_PAYLOAD_SIZE ) )
+		return 0;
+
+	// add total block size of the next block to the blockH's payload_size
+	blockH->payload_size = blockH->payload_size + get_total_block_size(next_blockH);
 
 	// remove next_block from the linkedlist
-	remove_from_linkedlist(&blocks_list, next_block);
+	remove_from_linkedlist(&blocks_list, next_blockH);
 
 	return 1;
+}
+
+// binary search tree to link free nodes, so we can find best fit free nodes in O(log(n))
+static bst free_tree;
+
+// returns 1, if a block is free
+static int is_free_block(const block_header* blockH)
+{
+	return (!is_new_bstnode(&free_tree, &(blockH->free_node))) || (free_tree.root == &(blockH->free_node));
 }
 
 void allok_init(int debugL)
@@ -205,27 +210,33 @@ void* allok(size_t size)
 
 void freek(void* mptr)
 {
+	if(mptr == NULL)
+		return;
+
 	block_header* blockH = (mptr - sizeof(block_header));
 
 	if(is_free_block(blockH))
 		return;
 
-	if(blockH->payload_size < MAX_PAYLOAD_SIZE)
+	// perform merge of blockH with its next and previous adjacent blocks
+	// blockH must hold the address of the largest block formed after merging
 	{
-		block_header* next_block = (block_header*) get_next_of(&blocks_list, blockH);
+		// try to merge blockH with its next adjacent block
+		block_header* next_block = (block_header*) get_next_block_of(blockH);
 		if(next_block != NULL && is_free_block(next_block) && merge(blockH))
 			remove_from_bst(&free_tree, next_block);
 
-		block_header* prev_block = (block_header*) get_prev_of(&blocks_list, blockH);
+		// try to merge blockH with its previous adjacent block
+		block_header* prev_block = (block_header*) get_prev_block_of(blockH);
 		if(prev_block != NULL && is_free_block(prev_block) && merge(prev_block))
 		{
 			remove_from_bst(&free_tree, prev_block);
 			blockH = prev_block;
 		}
-
-		insert_in_bst(&free_tree, blockH);
 	}
 	
 	if(blockH->payload_size == MAX_PAYLOAD_SIZE)
 		delete_used_block(blockH);
+	else
+		insert_in_bst(&free_tree, blockH);
 }
