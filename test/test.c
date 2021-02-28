d9ff264fc7d96ddae5379a8ff675de19ddb06257
Rohan Dvivedi <rohandvivedi@gmail.com>
2021-02-28 12:29:30 +0530

    using debug print functions in  the test cases

--- test/test.c
+++ test/test.c
@@ -3,10 +3,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define TEST_ALLOKS 197
+#define TEST_ALLOKS 300
 
-unsigned int test_sizes_size = 5;
-unsigned int test_sizes[] = {6, 16, 20, 35, 55};
+unsigned int ALLOCATION_RUNS_COUNT = 3;
+unsigned int ALLOK_RUNS_SIZES[] = {100, 100, 100};
+unsigned int FREEK_RUNS_SIZES[] = { 60,  90, 150};
+
+unsigned int test_sizes_size = 6;
+unsigned int test_sizes[] = {8, 16, 20, 40, 64, 96};
 
 int memory_sizes[TEST_ALLOKS];
 void* memory[TEST_ALLOKS];
@@ -14,28 +18,33 @@ void* memory[TEST_ALLOKS];
 int main()
 {
 	unsigned int total_allocation_size = 0;
-	allok_init(1);
 
 	for(unsigned int i = 0; i < TEST_ALLOKS; i++)
 	{
 		memory_sizes[i] = test_sizes[ rand() % test_sizes_size ];
-		printf("%d - Allocating memory for size %u\n", i, memory_sizes[i]);
-		memory[i] = allok(memory_sizes[i]);
-		printf("%d - Allocated memory for size %u at %p\n\n", i, memory_sizes[i], memory[i]);
-
 		total_allocation_size += memory_sizes[i];
 	}
 
-	printf("Allocations Completed\n");
+	printf("Total usable memory size: %u\n\n", total_allocation_size);
 
-	printf("Total usable memory size: %u\n", total_allocation_size);
+	allok_init();
+	printf("Mememory allocator initialized\n\n");
 
 	for(unsigned int i = 0; i < TEST_ALLOKS; i++)
 	{
-		printf("%d - Freeing memory for size %u at %p\n", i, memory_sizes[i], memory[i]);
+		memory[i] = allok(memory_sizes[i]);
+		printf("%d - Allocated memory for size %u at %p\n\n", i, memory_sizes[i], memory[i]);
+	}
+
+	debug_print_allok();
+
+	for(unsigned int i = 0; i < TEST_ALLOKS; i++)
+	{
 		freek(memory[i]);
 		printf("%d - Freed memory for size %u at %p\n\n", i, memory_sizes[i], memory[i]);
 	}
+
+	debug_print_allok();
 
 	return 0;
 }
